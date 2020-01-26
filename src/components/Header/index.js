ad1a6e8afaf3893eb70d483456acc8ebe013665a
CristianoLimaOliveira <59293960+CristianoLimaOliveira@users.noreply.github.com>
2020-01-26 03:10:23 -0300

    FrontEnd da tela de cadastro concluído.

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+    <div className="header">
+        <a href="btn_entrar">Entrar</a>
+    </div>
+);
+
+export default Header;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './styles.css';
+
+const TelaInicial = () => (
+    <div className="telaInicial">
+        <register id="registro">
+            <div className="forms">
+                <label id="lbl_nome">Nome:</label>
+                <input id="txt_nome" type="text"></input>
+            </div>
+            <div className="forms">
+                <label id="lbl_email">E-mail:</label>
+                <input id="txt_email" type="text"></input>
+            </div>
+            <div className="forms">
+                <label id="lbl_senha">Senha:</label>
+                <input id="txt_senha" type="text"></input>
+            </div>
+            <div className="forms">
+                <label id="lbl_confirmarSenha">Confirmar Senha:</label>
+                <input id="txt_confirmarSenha" type="text"></input>
+            </div>
+            <div className="forms">
+                <a href="btn_registrar">Registrar</a>
+            </div>
+        </register>
+    </div>
+);
+
+export default TelaInicial;
