ac5ecd702c6e7e67e3e672dfe985d07310d42119
GabrielF9 <gabrielf.dev16@gmail.com>
2020-01-29 09:04:57 -0300

    feat: backend finished

--- frontend/src/components/CorpoTelaInicial/index.js
+++ frontend/src/components/CorpoTelaInicial/index.js
@@ -5,7 +5,7 @@ const CorpoTelaInicial = () => (
     <div className="corpoTelaInicial">
         <div id="telaInicial">
             <a href="/alterar">Modificar</a>
-            <a href="btn_listar">Listar</a> {/* TODO: Criar tela de listagem */}
+            <a href="/listagem">Listar</a> {/* TODO: Criar tela de listagem */}
             <a href="/buscar">Buscar</a>
         </div>
     </div>
--- frontend/src/components/CorpoTelaListagem/index.js
+++ frontend/src/components/CorpoTelaListagem/index.js
@@ -1,96 +1,12 @@
 import React from 'react';
 import './styles.css';
 
-const CorpoTelaListagem = () => {
-  return (
-    <div className="container">
-      <div className="corpoTelaListagem">
-        <div id="listagem">
-          <div className="forms">
-            <label id="lbl_email">E-mail:</label>
-            <input id="txt_email" type="text"></input>
-          </div>
-          <div className="forms">
-            <label id="lbl_senha">Senha:</label>
-            <input id="txt_senha" type="text"></input>
-          </div><div className="forms">
-            <a href="/inicio" >Entrar</a>
-          </div>
-        </div>
-      </div>
-      <div className="corpoTelaListagem">
-        <div id="listagem">
-          <div className="forms">
-            <label id="lbl_email">E-mail:</label>
-            <input id="txt_email" type="text"></input>
-          </div>
-          <div className="forms">
-            <label id="lbl_senha">Senha:</label>
-            <input id="txt_senha" type="text"></input>
-          </div><div className="forms">
-            <a href="/inicio" >Entrar</a>
-          </div>
-        </div>
-      </div>
-      <div className="corpoTelaListagem">
-        <div id="listagem">
-          <div className="forms">
-            <label id="lbl_email">E-mail:</label>
-            <input id="txt_email" type="text"></input>
-          </div>
-          <div className="forms">
-            <label id="lbl_senha">Senha:</label>
-            <input id="txt_senha" type="text"></input>
-          </div><div className="forms">
-            <a href="/inicio" >Entrar</a>
-          </div>
-        </div>
-      </div>
-      <div className="corpoTelaListagem">
-        <div id="listagem">
-          <div className="forms">
-            <label id="lbl_email">E-mail:</label>
-            <input id="txt_email" type="text"></input>
-          </div>
-          <div className="forms">
-            <label id="lbl_senha">Senha:</label>
-            <input id="txt_senha" type="text"></input>
-          </div><div className="forms">
-            <a href="/inicio" >Entrar</a>
-          </div>
-        </div>
-      </div>
-      <div className="corpoTelaListagem">
-        <div id="listagem">
-          <div className="forms">
-            <label id="lbl_email">E-mail:</label>
-            <input id="txt_email" type="text"></input>
-          </div>
-          <div className="forms">
-            <label id="lbl_senha">Senha:</label>
-            <input id="txt_senha" type="text"></input>
-          </div><div className="forms">
-            <a href="/inicio" >Entrar</a>
-          </div>
-        </div>
-      </div>
-      <div className="corpoTelaListagem">
-        <div id="listagem">
-          <div className="forms">
-            <label id="lbl_email">E-mail:</label>
-            <input id="txt_email" type="text"></input>
-          </div>
-          <div className="forms">
-            <label id="lbl_senha">Senha:</label>
-            <input id="txt_senha" type="text"></input>
-          </div><div className="forms">
-            <a href="/inicio" >Entrar</a>
-          </div>
-        </div>
-      </div>
-      <div className='divider'></div>
+const CorpoTelaListagem = () => (
+  <div className="corpoTelaListagem">
+    <div id="telaListagem">
+      <h1>Aqui temos planetas</h1>
     </div>
-  );
-};
+  </div>
+);
 
 export default CorpoTelaListagem;
